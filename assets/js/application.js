2172b72f0215f138d6504fc485f0031bf238b163
Robb Shecter <robb@public.law>
2023-11-24 22:25:46 -0700

    checkpoint

--- assets/js/application.js
+++ assets/js/application.js
@@ -9,6 +9,7 @@ $(function() {
     $(this).find('a').colorbox({
         maxWidth : '80%',
         maxHeight : '80%',
+        opacity : 0.8,
         transition : 'elastic',
         current : ''
     })
